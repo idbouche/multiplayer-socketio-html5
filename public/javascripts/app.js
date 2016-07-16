9d67408a0f3f8fc7c465af3c39d23de1b59c1ef7
idb <idb@MacBook-Air-de-idb.local>
2016-07-16 11:38:21 +0200

    collision

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,129 @@
-//$(document).foundation()
-
+var canvas  = document.getElementById('cv');
+var cv      = canvas.getContext("2d");
+var ctx     = canvas.getContext("2d");
+var text    = canvas.getContext("2d");
+var form    = document.getElementById('form');
+var nom1    = document.getElementById('nom1');
+var nom2    = document.getElementById('nom2');
+var tIme    = document.getElementById('tImes');
+var go    = document.getElementById('go');
 
 var socket = io();
- socket.on('message', function(message) {
-        
-        console.log(message.yes);
-    })
- socket.emit('happy',{left:'false'});
+
+
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    var nom    = document.getElementById('nom').value;
+    if(nom !== ''){
+
+        socket.emit('start',{user: nom,  state:true});
+        //socket.emit('canvas',{heigth: nom, color: color, state:true});
+    }
+});
+
+go.addEventListener('click',function () {
+   form.style.display = 'none';
+   //style.display = "none";
+
+})
+
+
+
+var loop = function(data){
+    for(var i = 0 ; i < data.length; i++){
+
+        if (i === 0 ){
+
+        	cv.fillRect(data[i].x,data[i].y,data[i].width,data[i].height);
+
+            ctx.fillRect(data[i].Bx,data[i].By+35,25,25)
+            ctx.fillStyle = 'black';
+
+            nom1.innerHTML = 'Nom : '+ data[i].User + " <span>Score : "+ data[i].score2 + '</span>'
+        }else {
+            cv.fillRect(780,data[i].y,data[i].width,data[i].height);
+
+            nom2.innerHTML = 'Nom : '+ data[i].User + " <span>Score : " + data[i].score1 + '</span>'
+    	 }
+
+    }
+}
+
+socket.on('newPositions',function(data){
+    cv.clearRect(0,0,800,500);
+    ctx.beginPath();
+    ctx.moveTo(400,0);
+    ctx.lineTo(400,500);
+    ctx.stroke();
+    loop(data);
+
+
+});
+
+socket.on('victoir1',function(data){
+
+    console.log(data.victoir1);
+
+
+});
+
+socket.on('victoir2',function(data){
+
+   console.log(data.victoir2);
+
+});
+
+socket.on('state',function(data){
+   if (data.state) {
+
+      var cont = 3;
+      var id = setInterval(function(){
+         console.log(cont);
+         tImer.innerHTML = cont;
+         cont--;
+         if (cont === 0){
+            tImer.innerHTML = '';
+            socket.emit('state',{state:true});
+            clearInterval(id);
+         }
+
+      }, 2000);
+
+   }
+
+});
+
+ socket.on('listattent',function(data){
+    for(var i = 0 ; i < data.length; i++){
+        var list = document.getElementById('list');
+        list.innerHTML +='<p>'+data[i].user+'</p>';
+        console.log(data[i].user)
+
+    }
+ });
+
+socket.on('cont', function(data){
+    console.log(data.timer);
+    ctx.font = "70px Arial";
+    ctx.fillText(data.timer,400,250);
+})
+
+document.onkeydown = function(event){
+    if(event.keyCode === 40)
+        socket.emit('keyPress',{inputId:'down',state:true});
+    else if(event.keyCode === 38)
+        socket.emit('keyPress',{inputId:'up',state:true});
+    else if(event.keyCode === 32)
+        socket.emit('keyPress',{inputId:'start',state:true});
+}
+
+    document.onkeyup = function(event){
+        if(event.keyCode === 40)
+            socket.emit('keyPress',{inputId:'down',state:false});
+        else if(event.keyCode === 38)
+            socket.emit('keyPress',{inputId:'up',state:false});
+        // else if(event.keyCode === 32)
+        //     socket.emit('keyPress',{inputId:'start',state:false});
+    }
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,80 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server,{});
 
-
 /* GET home page. */
 
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+var ATTENT_LIST = {};
+var start       =false;
+var score1      = 0;
+var score2      = 0;
+var victoir1      = 0;
+var victoir2      = 0;
 
+var Player = function(id){
+    var play = {
+        x:10,
+        y:10,
+        id:id,
+        user :'',
+        width:10,
+        height:100,
+        pressingUp:false,
+        pressingDown:false,
+        maxSpd:10,
+
+
+    }
+       play.updatePosition = function(){
+        if(play.pressingUp)
+            play.y -= play.maxSpd;
+        if(play.pressingDown)
+            play.y += play.maxSpd;
+
+        if (play.y + 90 >= 495 ) {
+            play.y = 410;
+            }
+        if ( play.y < 0) {
+            play.y = 0;
+            }
+
+    }
+    return play;
+}
+
+
+var Ball = function(){
+    var ball = {
+        x:387,
+        y:240,
+        width:20,
+        height:20,
+        pressingStart:false,
+        vx:2,
+        vy:1,
+        state:false,
+    }
+
+    ball.updatePosition = function(){
+
+        if (ball.state){
+            ball.x += ball.vx;
+            ball.y += ball.vy;
+
+            if (ball.y+ ball.vy >= 450 || ball.y + ball.vy < -30) {
+                ball.vy = -ball.vy;
+            }
+            if (ball.x + ball.vx > 780 || ball.x + ball.vx < 0) {
+                ball.vx = -ball.vx;
+            }
+        }
+    }
+
+    return ball;
+}
+
+var ball = Ball()
 
 
 module.exports = function(io) {
@@ -15,35 +85,152 @@ module.exports = function(io) {
     var router = app.Router();
 
     router.get('/', function(req, res, next) {
-  		res.render('index', { title: 'Express' });
-	});
-    var ok ;
-    io.on('connection', function(socket) { 
-        console.log('CONNECTION');
-        
-        socket.on('happy', function(data) { 
-        	console.log(data.left); 
-            if(data.left == 'true'){
-                ok =  'ok'
-                console.log('9',data.left); 
-                socket.emit('message',{'yes': ok});
-            }else{
-                ok = 'ko'
-                socket.emit('message',{'yes': ok});
-            }
-
-    	});
-    	//socket.emit('message',{'yes': ok});
+  		res.render('index', { title: 'Game multiplayers' });
     });
 
+    io.on('connection', function(socket) {
+
+        var arr = Object.keys(PLAYER_LIST).map(function (key) {return PLAYER_LIST[key]});
+
+        socket.id = Math.random();
+        SOCKET_LIST[socket.id] = socket;
+
+        var size = Object.keys(PLAYER_LIST).length;
+
+        var player = Player(socket.id);
+        var n = 0;
+        socket.on('start',function(dat){
+            if (dat.user !== ''){
+                if(size<2){
+                    PLAYER_LIST[socket.id] = player;
+                    if (size === 1) {
+                        socket.emit('state',{state:true});
+
+                    }else {
+                        socket.emit('state',{state:false});
+                    }
+                    n++;
+                }else{
+                    ATTENT_LIST[socket.id] = player;
+
+                }
 
 
+            }
+            player.user  =  dat.user;
+        });
+
+         socket.on('state',function (data) {
+             if (data.state){
+                 ball.state = true
+             }else {
+                ball.state = false
+             }
+
+         })
+
+        console.log(PLAYER_LIST)
+        console.log(size)
+
+        var arrlist = Object.keys(ATTENT_LIST).map(function (key) {return ATTENT_LIST[key]});
+        if (arrlist.length > 0){
+            socket.emit('listattent',arrlist);
+            console.log('list'+arrlist);
+        }
+        socket.on('disconnect',function(){
+            delete SOCKET_LIST[socket.id];
+            delete PLAYER_LIST[socket.id];
+            delete ATTENT_LIST[socket.id];
+            delete ball
+            });
+
+        socket.on('keyPress',function(data){
+            if(data.inputId === 'up')
+                player.pressingUp = data.state;
+            else if(data.inputId === 'down')
+                player.pressingDown = data.state;
+            else if(data.inputId === 'start')
+                ball.pressingStart = data.state;
+        });
+
+
+    });
     return router;
 }
 
+var nouvoEmmit = function(cont,socket){
+    socket.emit('cont',{timer:cont} );
+
+}
+
+ /* Gestion de collision  ******************************************************/
+var rangeIntersect = function(min0, max0, min1, max1) {
+    return Math.max(min0, max0) >= Math.min(min1, max1) &&
+        Math.min(min0, max0) <= Math.max(min1, max1);
+}
+
+var colision = function(r0, r1) {
+    return rangeIntersect(r0.x, r0.x + r0.width, r1.x, r1.x + r1.width) &&
+        rangeIntersect(r0.y, r0.y + r0.height, r1.y, r1.y + r1.height);
+}
+/*Fin gestion collision  ******************************************************/
+
+setInterval(function(){
+    var pack = [];
+
+    for(var i in PLAYER_LIST){
+        ball.updatePosition();
+        var player = PLAYER_LIST[i];
+        player.updatePosition();
+        pack.push({
+            x:player.x,
+            y:player.y,
+            width:player.width,
+            height:player.height,
+            User:player.user,
+            Bx:ball.x,
+            By:ball.y,
+            score1:score1,
+            score2:score2,
+        });
+
+        if (pack[0] !== undefined && pack[1] !== undefined){
+
+            if (i !== 0){
+                player.x=760;
+            }
+            if (colision(pack[0],ball)){
+                     ball.vx = -ball.vx;
+                 }
+            if (colision(pack[1],ball)){
+                     ball.vx = -ball.vx;
+                 }
+            if (ball.x > 773) {
+                score2 ++
+                ball.x = 400
+                ball.y = 250
+            }
+            if (ball.x < 6 ) {
+                score1 ++
+                ball.x = 400
+                ball.y = 250
+            }
+        }
+    }
 
 
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.emit('newPositions',pack);
+        if (score1 == 11 && (score1 - score2)>= 2 ){
+            victoir1++;
+            socket.emit('victoir1',{victoir1:victoir1, gamesOver:true} );
+        }
 
+        if (score2 == 11 && (score2 - score1)>= 2 ){
+            victoir2++;
+            socket.emit('victoir2',{victoir2:victoir2, gamesOver:true} );
+        }
+    }
 
-
-
+},1000/25);
